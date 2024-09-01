0f95a060092f6ce27748095f4795c3109d40d978
walid oumechtak <woumechtak@gmail.com>
2024-09-01 18:15:31 +0100

    start create add tenant modal

---
+++ frontend/src/components/AddTenantModal.tsx
@@ -0,0 +1,140 @@
+import { useGeneralStore } from "../stores/generalStore";
+import { ImCross } from "react-icons/im";
+import Input from "./Inupt";
+import { useState } from "react";
+import { GraphQLErrorExtensions } from "graphql";
+import { CreatePropertieMutation } from "../gql/graphql";
+import { CREATE_PROPERTIE } from "../graphql/mutations/CreatePropertie";
+import { useMutation } from "@apollo/client";
+import { useUserStore } from "../stores/userStore";
+
+function PropertieModal() {
+    const setIsPropertyModalOpen = useGeneralStore((state) => state.setIsPropertyModalOpen);
+    const isPropertyModalOpen = useGeneralStore((state) => state.isPropertyModalOpen);
+    const [errors, setErrors] = useState<GraphQLErrorExtensions>({});
+    const [addPropertie, {data, error, loading}] = useMutation<CreatePropertieMutation>(CREATE_PROPERTIE);
+    const [tenantData, setTenantData] = useState({
+        name: "",
+        contact: "",
+        section: ""
+    });
+    const user = useUserStore();
+
+    const handleAddPropertie = async () => {
+        setErrors({});
+        const property = await addPropertie({
+            variables: {
+                name: tenantData.name,
+                contact: tenantData.contact,
+                section: tenantData.section,
+            },
+            refetchQueries: ["GetTenants"],
+            onCompleted: () => {
+                console.log("Propertie added: --->", data);
+            },
+        }).catch(err => {
+            setErrors(err.graphQLErrors[0].extensions);
+        })
+        if (property) {
+            setIsPropertyModalOpen(!isPropertyModalOpen);
+            console.log("Isproperty Modal open:" ,isPropertyModalOpen)
+        }
+    }
+    
+    return (
+        <div
+            id="AuthModal"
+            className="fixed flex items-center justify-center w-full h-full bg-black bg-opacity-50
+                       z-50 top-0 left-0"
+            >
+            <div
+                className="bg-white relative w-full max-w-[480px] h-[70%] p-4 rounded-lg"
+            >
+                <div
+                    className="w-full flex justify-end"
+                >
+                    <button onClick={() => {setIsPropertyModalOpen(!isPropertyModalOpen)}}>
+                        <ImCross size={"17"} color="balck"/>
+                    </button>
+                </div>
+            <div className="text-center text-[28px] mb-4 font-bold">Add Propertie</div>
+            <div className="px-6 pb-6">
+                <Input 
+                    placeHolder="Name"
+                    InputType="text"
+                    max={64}
+                    error={errors?.name as string}
+                    onChange={(e) => setAddPropertieData({...addPropertieData, name: e.target.value})}
+                    autoFocus={false}
+                />
+            </div>
+            <div className="px-6 pb-6">
+                <Input 
+                    placeHolder="Address"
+                    InputType="text"
+                    max={64}
+                    error={errors?.address as string}
+                    onChange={(e) => setAddPropertieData({...addPropertieData, address: e.target.value})}
+                    autoFocus={false}
+                />
+            </div>
+            <div className="px-6 pb-6">
+                <Input 
+                    placeHolder="Type (e.g. Apartment, House)"
+                    InputType="text"
+                    max={64}
+                    error={errors?.type as string}
+                    onChange={(e) => setAddPropertieData({...addPropertieData, type: e.target.value})}
+                    autoFocus={false}
+                />
+            </div>
+            <div className="px-6 pb-6">
+                <Input 
+                    placeHolder="Number of Units"
+                    InputType="number"
+                    max={64}
+                    error={errors?.numberOfUnits as string}
+                    onChange={(e) => setAddPropertieData({...addPropertieData, numberOfUnits: parseInt(e.target.value)})}
+                    autoFocus={false}
+                />
+            </div>
+            <div className="px-6 pb-6">
+                <Input 
+                    placeHolder="Rental Cost"
+                    InputType="number"
+                    max={64}
+                    error={errors?.rentalCost as string}
+                    onChange={(e) => setAddPropertieData({...addPropertieData, rentalCost: parseInt(e.target.value)})}
+                    autoFocus={false}
+                />
+            </div>
+            <div className="mx-6 mt-2">
+                <button
+                    disabled={
+                        !addPropertieData.name ||
+                        !addPropertieData.address ||
+                        !addPropertieData.type ||
+                        addPropertieData.numberOfUnits === 0 ||
+                        addPropertieData.rentalCost === 0
+                    }
+                    onClick={handleAddPropertie}
+                    className={[
+                        "w-full text-[17px] font-semibold text-white py-3 rounded-sm",
+                        !addPropertieData.name ||
+                        !addPropertieData.address ||
+                        !addPropertieData.type ||
+                        addPropertieData.numberOfUnits === 0 ||
+                        addPropertieData.rentalCost === 0
+                          ? "bg-gray-200"
+                          : "bg-[#F02C56]",
+                      ].join(" ")}
+                      >
+                    Add Propertie
+                </button>   
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default PropertieModal;
--- frontend/src/pages/SinglePropertie.tsx
+++ frontend/src/pages/SinglePropertie.tsx
@@ -3,6 +3,9 @@ import propertyImage from "../assets/images/Property.jpg";
 import { useQuery } from "@apollo/client";
 import { GetPropertieQuery, GetPropertieQueryVariables } from "../gql/graphql";
 import { GET_PROPERTIE } from "../graphql/queries/getPropertie";
+import { FaDollarSign, FaMapMarkerAlt } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+import { IoReorderThreeOutline } from "react-icons/io5";
 
 function SinglePropertie() {
     const id = window.location.pathname.split("/").pop();
@@ -15,15 +18,37 @@ function SinglePropertie() {
     return (
         <MainLayout>
           <div className="bg-gray-50 h-[100vh] w-full pt-[80px] ">
-              <div className="flex mx-auto container bg-white max-w-[1200px] shadow-custom">
-                  <div className="w-1/2 mr-2">
-                    <img src={propertyImage} alt="" />
+              <div className="flex flex-col mx-auto md:flex-row container bg-white max-w-[1200px] shadow-custom">
+                  <div className="md:w-1/2 w-full mr-2">
+                    <img src={propertyImage} className="w-full h-full" alt="" />
                   </div>
-              <div className="w-1/2 flex text-center p-4 flex-col">
-                  <div>
-                    <h2 className="font-bold text-[24px]">{data?.getPropertie.name}</h2>
+                  <div className="md:w-1/2 w-full flex p-8 flex-col">
+                    <div className="mb-4">
+                      <h2 className="font-bold text-[24px]">{data?.getPropertie.name}</h2>
+                    </div>
+                    <div className="flex items-center mb-2">
+                      <FaMapMarkerAlt className="mr-2" color="#9ca3af"/>
+                      <p className="text-[18px] text-gray-400">Address: {data?.getPropertie.address}</p>
+                    </div>
+                    <div className="flex items-center mb-2">
+                      <FaHome className="mr-2" color="#9ca3af"/>
+                      <p className="text-[18px] text-gray-400">Type: {data?.getPropertie.type}</p>
+                    </div>
+                    <div className="flex items-center mb-2">
+                      <IoReorderThreeOutline  className="mr-2" color="#9ca3af"/>
+                      <p className="text-[18px] text-gray-400">Number of Units: {data?.getPropertie.numberOfUnits}</p>
+                    </div>
+                    <div className="flex items-center mb-2">
+                      <FaDollarSign className="mr-2" color="#9ca3af"/>
+                      <p className="text-[18px] text-gray-400">Rental Cost: {data?.getPropertie.rentalCost}$ /month</p>
+                    </div>
+                    <div className="w-full flex justify-center mt-5">
+                        <button
+                          className="bg-red-500 text-white px-6 py-4 font-semibold rounded-sm border border-red-500 hover:bg-white hover:text-red-500">
+                          Add new tenant  
+                        </button>
+                    </div>
                   </div>
-              </div>
               </div>
           </div>
         </MainLayout>
